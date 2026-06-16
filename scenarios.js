b12506827ebfb4c59a95b700cedd7e83be2e3dbe
saqlvation <volpetta0987@gmail.com>
2026-06-16 13:53:29 +0200

    makes it start by the anime array

--- scenarios.js
+++ scenarios.js
@@ -1,4 +1,5 @@
 const scenarios = [
-    {name : "Animals", currentCharacters: animals, background: "#dd933f44"},
-    {name : "Anime", currentCharacters: anime, background: "#197dcf44"}
+    {name : "Anime", currentCharacters: anime, background: "#197dcf44"},
+    {name : "Animals", currentCharacters: animals, background: "#dd933f44"}
+    
 ]
